b1c8224bf977d92367bdfc2ce33c7aff55e62b36
tiedie211 <tchristiand@gmail.com>
2017-04-02 13:14:17 -0700

    Hello Window Tutorial

--- LOGLEngine/Sources/main.cpp
+++ LOGLEngine/Sources/main.cpp
@@ -9,40 +9,58 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main(int argc, char * argv[]) {
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    // When a user presses the escape key, we set the WindowShouldClose property to true,
+    // closing the application
+    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
 
-    // Load GLFW and Create a Window
+int main() {
+
+    // Load GLFW and create a window
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
-    auto mWindow = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
+    auto window = glfwCreateWindow(mWidth, mHeight, "OpenGL", nullptr, nullptr);
 
-    // Check for Valid Context
-    if (mWindow == nullptr) {
+    // Check for valid context
+    if (!window) {
         fprintf(stderr, "Failed to Create OpenGL Context");
         return EXIT_FAILURE;
     }
 
-    // Create Context and Load OpenGL Functions
-    glfwMakeContextCurrent(mWindow);
+    // Create context and load OpenGL functions
+    glfwMakeContextCurrent(window);
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
-    // Rendering Loop
-    while (glfwWindowShouldClose(mWindow) == false) {
-        if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-            glfwSetWindowShouldClose(mWindow, true);
+    // Register callbacks for events
+    glfwSetKeyCallback(window, key_callback);
+    
+    // Rendering loop
+    while (!glfwWindowShouldClose(window)) {
+        // Check and call events
+        glfwPollEvents();
 
-        // Background Fill Color
+        // Background fill color
         glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        // Set viewport size for rendering
+        int width, height;
+        glfwGetFramebufferSize(window, &width, &height);
+        glViewport(0, 0, width, height);
 
-        // Flip Buffers and Draw
-        glfwSwapBuffers(mWindow);
-        glfwPollEvents();
-    }   glfwTerminate();
+        // Swap the buffers
+        glfwSwapBuffers(window);
+    }
+    
+    // Destroy the window
+    glfwTerminate();
     return EXIT_SUCCESS;
 }
